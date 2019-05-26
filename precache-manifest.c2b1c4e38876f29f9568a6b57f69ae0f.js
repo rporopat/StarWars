d8539d0179aa6f9020eb17f9becc5ef717e5d1d5
rporopat <poropat.roberto@gmail.com>
2019-05-26 15:04:51 +0200

    Updates

---
+++ precache-manifest.c2b1c4e38876f29f9568a6b57f69ae0f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46cf2bdb61dc8717a404fd73472cded7",
+    "url": "/StarWars/index.html"
+  },
+  {
+    "revision": "ef0087dfea0d81ac284d",
+    "url": "/StarWars/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "d0c9c8ae20eb3788b237",
+    "url": "/StarWars/static/css/main.ecc419dd.chunk.css"
+  },
+  {
+    "revision": "ef0087dfea0d81ac284d",
+    "url": "/StarWars/static/js/2.b0408cd9.chunk.js"
+  },
+  {
+    "revision": "d0c9c8ae20eb3788b237",
+    "url": "/StarWars/static/js/main.36ded4f8.chunk.js"
+  },
+  {
+    "revision": "f3d474882ea9de1a49af",
+    "url": "/StarWars/static/js/runtime~main.41ad9c12.js"
+  },
+  {
+    "revision": "ade91cb0bb7c69696a7df18125ae82cb",
+    "url": "/StarWars/static/media/Death Star.ade91cb0.otf"
+  },
+  {
+    "revision": "883a1ed70178a3dfd30c3d512ffa5379",
+    "url": "/StarWars/static/media/Tatooine.883a1ed7.png"
+  },
+  {
+    "revision": "6494ab5b3760b5e2cc46f5566a245371",
+    "url": "/StarWars/static/media/stars.6494ab5b.png"
+  }
+]);
